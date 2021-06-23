0284642b95317f4ea1dea6c8921c1ebdf75acc7a
Leandro <leandrofernandesvieira@gmail.com>
2021-06-23 19:00:51 -0300

    refactor: update streaming dns

--- client/src/contants/index.ts
+++ client/src/contants/index.ts
@@ -1,2 +1,2 @@
-export const STREAMING_URL = "https://adai.online.church/";
+export const STREAMING_URL = "https://aovivo.conferenciaflores.com.br";
 export const AUTH_API_URL = "https://auth.conferenciaflores.com.br";
